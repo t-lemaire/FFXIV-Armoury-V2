5a27d8c48afcd2cecb626cadf12d492e5c10e9c6
Tlemaire <terry.lemaire@gmail.com>
2022-02-21 19:20:22 -0500

    Starts implementing gear list view without data.

---
+++ FFXIV Armoury V2/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using FFXIV_Armoury_V2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIV_Armoury_V2.MVVM.ViewModel
+{
+    class MainViewModel: ObservableObject
+    {
+        public RelayCommand GearListVewCommand { get; set; }
+
+        public GearListViewModel GearListVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            GearListVM = new GearListViewModel();
+
+            CurrentView = GearListVM;
+
+            GearListVewCommand = new RelayCommand(o =>
+            {
+                CurrentView = GearListVM;
+            });
+        }
+    }
+}
