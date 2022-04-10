99aa4ccca5897496c041b6218411e67a4958dcbc
Tlemaire <terry.lemaire@gmail.com>
2022-04-10 17:28:06 -0400

    Issue #5 Starts implementing user control for toggle image used in gear items list filtering.

---
+++ FFXIV Armoury V2/UserControls/ImageToggle.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FFXIV_Armoury_V2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ImageToggle.xaml
+    /// </summary>
+    public partial class ImageToggle : UserControl
+    {
+        /*private string _toggledImage;
+
+        public string ToggledImage
+        {
+            get { return _toggledImage; }
+            set { _toggledImage = value; }
+        }*/
+
+        private static DependencyProperty _untoggledImage = DependencyProperty.Register("UntoggledImage", typeof(string), typeof(ImageToggle));
+
+        public string UntoggledImage
+        {
+            get { 
+                return (string)GetValue(_untoggledImage);
+            }
+            set { 
+                SetValue(_untoggledImage, value);
+            }
+        }
+
+        private static DependencyProperty _tooltip = DependencyProperty.Register("Tooltip", typeof(string), typeof(ImageToggle));
+
+        public string Tooltip
+        {
+            get
+            {
+                return (string)GetValue(_tooltip);
+            }
+            set
+            {
+                SetValue(_tooltip, value);
+            }
+        }
+
+        /*private bool _isToggled;
+
+        public bool IsToggled
+        {
+            get { return _isToggled; }
+            set { _isToggled = value; }
+        }*/
+
+
+        public ImageToggle()
+        {
+            InitializeComponent();
+        }
+    }
+}
